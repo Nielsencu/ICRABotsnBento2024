1cf8ffdaabd123b9c88d274c17bd29ac29add70e
Nielsen Cugito <nielsencugito@gmail.com>
2024-05-15 05:51:04 +0800

    added front and back camera artag callback

--- src/base/localization.py
+++ src/base/localization.py
@@ -10,37 +10,46 @@ from sensor_msgs.msg import JointState, CompressedImage, Imu
 from rclpy.qos import qos_profile_sensor_data
 from enum import Enum
 from global_planner import get_global_plan, get_global_plan_to_unexplored
-from collections import deque
+from tf_transformations import euler_from_quaternion
+from apriltag_msgs.msg import AprilTagDetectionArray
 
 MAP_WIDTH_M = 5
 MAP_HEIGHT_M = 5
 
-MAP_RES = 0.01 # in meters
+MAP_RES = 0.1 # in meters
 
 MAP_WIDTH_IN_PIXELS = int(MAP_WIDTH_M / MAP_RES)
 MAP_HEIGHT_IN_PIXELS = int(MAP_HEIGHT_M / MAP_RES)
 
+MAX_LIN_VEL = 0.15 # m/s
+MAX_ANG_VEL = 0.1 # rad/s 
+
 class RobotState(Enum):
     EXPLORATION = 0
     KLT_DETECTED = 1
-    GRIPPING = 2
-    PARKING = 3
-    FOLLOWING_GLOBAL_PATH = 4
+    PARKING = 2
 
-def get_position_from_artags_id(id) -> tuple[int, int]:
+def get_position_from_artags_id(id):
     id -= 101
-    x = int((id // 5) + 0.5)
-    y = int((id % 5) + 0.5)
+    x = (id // 5) + 0.5
+    y = (id % 5) + 0.5
     return (x, y)
 
-def is_valid_front_artag(id):
+def is_klt_artag(id):
     return 1 <= id <= 5
 
-def is_valid_ground_artag(id):
+def is_ground_artag(id):
     return 101 <= id <= 130
 
-def get_dt_from_consecutive_msg(msg, prev_msg):
-    return 
+def get_dt_from_consecutive_msg_in_secs(msg, prev_msg):
+    cur_time_ns = (msg.header.stamp.sec * 10e-9) + msg.header.stamp.nanosec
+    prev_time_ns = (prev_msg.header.stamp.sec * 10e-9) + prev_msg.header.stamp.nanosec
+    return (cur_time_ns - prev_time_ns) * 10e-9
+
+def get_l2_distance(pt1, pt2):
+    dx = pt1[0] - pt2[0]
+    dy = pt1[1] - pt2[1]
+    return 0.5**(dx*dx + dy*dy) 
 
 class MainNode(Node):
     def __init__(self, config_path, node_name):
@@ -49,24 +58,26 @@ class MainNode(Node):
         self.robot_map = np.zeros((MAP_WIDTH_IN_PIXELS, MAP_HEIGHT_IN_PIXELS))
         
         # Subscription to wheel encoder
-        self.joint_sub = self.create_subscription(JointState, 'joint_states', self.joint_state_cb, QoSProfile(depth=10))
+        self.joint_fl_sub = self.create_subscription(JointState, '/olive/servo/motor01/joint/state', self.joint_fl_state_cb, QoSProfile(depth=10))
+        self.joint_fr_sub = self.create_subscription(JointState, '/olive/servo/motor02/joint/state', self.joint_fr_state_cb, QoSProfile(depth=10))
+        self.joint_br_sub = self.create_subscription(JointState, '/olive/servo/motor03/joint/state', self.joint_br_state_cb, QoSProfile(depth=10))
+        self.joint_bl_sub = self.create_subscription(JointState, '/olive/servo/motor04/joint/state', self.joint_bl_state_cb, QoSProfile(depth=10))
         
-        self.imu_sub = self.create_subscription(Imu, 'imu', self.imu_cb, QoSProfile(depth=10))
+        self.imu_sub = self.create_subscription(Imu, '/olive/imu/imu06/filtered_imu', self.imu_cb, QoSProfile(depth=10))
         
         self.cmdvel_pub = self.create_publisher(Twist, 'cmd_vel', QoSProfile(depth=10))
         
-        # Subscribe to ground camera 
-        self.ground_camera_sub = self.create_subscription(
-            CompressedImage,
-            '/olive/camera/cam07/tags/image/compressed',
-            self.ground_camera_cb,
+        self.front_artag_sub = self.create_subscription(
+            AprilTagDetectionArray,
+            '/olive/camera/cam08/tags',
+            self.front_artag_cb,
             qos_profile=qos_profile_sensor_data
         )
         
-        self.front_camera_sub = self.create_subscription(
-            CompressedImage,
-            '/olive/camera/cam08/tags/image/compressed',
-            self.front_camera_cb,
+        self.back_artag_sub = self.create_subscription(
+            AprilTagDetectionArray,
+            '/olive/camera/cam07/tags',
+            self.back_artag_cb,
             qos_profile=qos_profile_sensor_data
         )
         
@@ -82,46 +93,62 @@ class MainNode(Node):
         self.x = 0.0
         self.y = 0.0
         
+        self.initial_yaw = 0.0
         self.yaw = 0.0
         
+        self.dFR, self.dBR, self.dBL, self.dFL = 0.0, 0.0, 0.0, 0.0
+        
         self.prev_imu_msg = None
-        self.prev_joint_msg = None
+        self.prev_fl_joint_msg = None
+        self.prev_fr_joint_msg = None
+        self.prev_bl_joint_msg = None
+        self.prev_br_joint_msg = None
+        
+        self.cur_fl_joint_msg = None
+        self.cur_fr_joint_msg = None
+        self.cur_bl_joint_msg = None
+        self.cur_br_joint_msg = None
         
         self.plan = []
         self.curPlanID = 0
         
-        self.has_global_plan = False
         self.has_completed_global_plan = False
         
         self.has_rotated_360 = False
         
         self.front_artag_id = -1
+        self.back_artag_id = -1
         
         # Set up the window for fullscreen or maximizable
         cv2.namedWindow("robotmap", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("robotmap", MAP_WIDTH_IN_PIXELS, MAP_HEIGHT_IN_PIXELS)  # Optional: Set an initial window size
         
-    def ground_camera_cb(self, msg):
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # Convert to OpenCV image
-        
-        # TODO: Retrieve ground artag id
-        
-        ground_artag_id = 0
-        
-        if is_valid_ground_artag(ground_artag_id):
-            # Set x and y to ground truth by looking at AR Tags
-            self.x, self.y = get_position_from_artags_id(ground_artag_id)
+    def front_artag_cb(self, msg):
+        if len(msg.detections) > 0:
+            print("Front camera detections!", len(msg.detections))
+            self.front_artag_id = msg.detections[0].id
             
-    def front_camera_cb(self, msg):
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # Convert to OpenCV image
-        
-        # TODO: Retrieve front artag id
-        front_artag_id = -1
-        
-        self.front_artag_id = front_artag_id
-        
+        if is_klt_artag(self.front_artag_id):
+            print("Valid KLT artag ID detected in front!", self.front_artag_id)
+        elif is_ground_artag(self.front_artag_id):
+            print("Valid Ground artag ID detected in front!", self.front_artag_id)
+            artag_position = get_position_from_artags_id(self.front_artag_id)
+            self.x, self.y = artag_position
+            print("Updated x, y", self.x, self.y)
+            
+    def back_artag_cb(self, msg):
+        if len(msg.detections) > 0:
+            print("Back camera detections!", len(msg.detections))
+            self.back_artag_id = msg.detections[0].id
+            
+        if is_klt_artag(self.back_artag_id):
+            print("Valid KLT artag ID detected at back!", self.back_artag_id)
+        elif is_ground_artag(self.back_artag_id):
+            print("Valid Ground artag ID detected at back!", self.back_artag_id)
+            artag_position = get_position_from_artags_id(self.back_artag_id)
+            self.x, self.y = artag_position
+            print("Updated x, y", self.x, self.y)
+    
     def set_state(self, state):
         self.state = state
         
@@ -135,6 +162,26 @@ class MainNode(Node):
     def get_cmd_vel_to_target_point(self, target_point):
         # TODO: Implement controtller to track target point (PID?)
         lin_x, ang_z = 0.0, 0.0    
+        
+        dy = target_point[1] - self.y
+        dx = target_point[0] - self.x
+        
+        kp = 0.5
+        
+        dist_to_target = (dy*dy + dx*dx) ** 0.5
+        
+        forward_err = kp * dist_to_target
+        
+        lin_x = min(forward_err, MAX_LIN_VEL)
+        
+        goal_yaw = np.arctan2(dy, dx)
+        
+        ang_kp = 0.3
+        
+        angular_err = ang_kp * (goal_yaw - self.yaw) 
+        
+        ang_z = max(min(angular_err, MAX_ANG_VEL), -MAX_ANG_VEL)
+        
         return (lin_x, ang_z)
     
     def follow_global_plan(self):
@@ -150,7 +197,6 @@ class MainNode(Node):
     def reset_global_plan(self):
         self.plan = []
         self.curPlanID = 0
-        
         self.has_completed_global_plan = False
 
     def thread_main(self):
@@ -158,13 +204,21 @@ class MainNode(Node):
         
         while not self.thread_exited:            
             time.sleep(1 / self.rate_control_hz)
+            
+            self.update_localization()
+            
             curPosInPixels = (self.x / MAP_RES, self.y / MAP_RES)
             
             print("Robot state : ", self.state)
             
             # For each of the robot states, publish cmd_vel
             if self.state == RobotState.EXPLORATION:
-                if not self.has_global_plan:
+                if is_klt_artag(self.front_artag_id) or is_klt_artag(self.back_artag_id):
+                    self.set_state(RobotState.KLT_DETECTED)
+                    self.reset_global_plan()
+                    continue
+                
+                if len(self.plan) == 0:
                     self.plan = get_global_plan_to_unexplored(curPosInPixels, self.robot_map)
                     self.curPlanID = 1
                 elif not self.has_completed_global_plan:
@@ -174,22 +228,25 @@ class MainNode(Node):
                     if self.has_rotated_360:
                         self.reset_global_plan()
                         continue
-                    elif is_valid_front_artag(self.front_artag_id):
-                        self.set_state(RobotState.KLT_DETECTED)
-                        self.reset_global_plan()
             elif self.state == RobotState.KLT_DETECTED:
                 # TODO: Try to orientate and find the best gripping position
                 ...
                 
             elif self.state == RobotState.PARKING:
-                if not self.has_global_plan:
+                if len(self.plan) == 0:
                     print("Front ARTag ID: ", self.front_artag_id)
-                    if is_valid_front_artag(self.front_artag_id):
+                    artag_id = -1
+                    if is_klt_artag(self.front_artag_id):
+                        artag_id = self.front_artag_id
+                    elif is_klt_artag(self.back_artag_id):
+                        artag_id = self.back_artag_id
+                    if artag_id == -1:
                         print("ARTag in the KLT not detected or not valid!!!, setting robot to exploration mode again")
-                        self.state = RobotState.EXPLORATION
+                        self.set_state(RobotState.EXPLORATION)
+                        self.reset_global_plan()
                         continue
                     
-                    parking_spot_id = self.front_artag_id + 100
+                    parking_spot_id = artag_id + 100
                     print("Going to parking spot id of ", parking_spot_id)
                     parkingGoalPos = get_position_from_artags_id(parking_spot_id)
                     print("Parking spot posiiton ", parkingGoalPos)
@@ -206,24 +263,71 @@ class MainNode(Node):
                     # Set robot state back to exploration for finding gripper
                     self.set_state(RobotState.EXPLORATION)    
                     self.reset_global_plan()                    
-            elif self.state == RobotState.GRIPPING:
-                # TODO: Command the servo motor to perform the gripping until successful
-                ...
     
     def imu_cb(self, msg):
         # TODO: Consider madgwick filter?
-        dt = get_dt_from_consecutive_msg(msg, self.prev_imu_msg)
-        self.yaw += msg.angular_velocity.z * dt
-        self.prev_imu_msg = msg
-
-    def joint_state_cb(self, msg):
-        dFL, dFR, dBR, dBL = 0.0, 0.0, 0.0, 0.0
-        if self.prev_joint_msg is not None:
-            dFR = msg.position[0] - self.prev_joint_msg.position[0]
-            dFL = msg.position[1] - self.prev_joint_msg.position[1]
-            dBR = msg.position[2] - self.prev_joint_msg.position[2]
-            dBL = msg.position[3] - self.prev_joint_msg.position[3]
+        dt = 0.0
+        quat = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        (_, _, yaw) = euler_from_quaternion(quat)
+        if self.prev_imu_msg is None:
+            self.initial_yaw = yaw
+        if self.prev_imu_msg is not None:
+            dt = get_dt_from_consecutive_msg_in_secs(msg, self.prev_imu_msg)
+            
+        # print("ang vel z is ", msg.angular_velocity.z)
+        # print("dt is ", dt )
+        # self.yaw = (self.yaw + (msg.angular_velocity.z * dt)) % np.pi
+        # print("Yaw is ", self.yaw)
         
+        self.yaw = yaw - self.initial_yaw
+        
+        print(self.get_cmd_vel_to_target_point((1,1)))
+        print("Yaw: ", self.yaw)
+        
+        self.prev_imu_msg = msg
+        
+    def joint_fr_state_cb(self, msg):
+        if self.prev_fr_joint_msg is not None:
+            self.dFR += msg.position[0] - self.prev_fr_joint_msg.position[0]
+        self.prev_fr_joint_msg = None
+        # self.cur_fr_joint_msg = msg
+        
+    def joint_br_state_cb(self, msg):
+        if self.prev_br_joint_msg is not None:
+            self.dBR += msg.position[0] - self.prev_br_joint_msg.position[0]
+        self.prev_br_joint_msg = None
+        # self.cur_br_joint_msg = msg
+        
+    def joint_bl_state_cb(self, msg):
+        if self.prev_bl_joint_msg is not None:
+            self.dBL += msg.position[0] - self.prev_bl_joint_msg.position[0]
+        self.prev_bl_joint_msg = None
+        # self.cur_bl_joint_msg = msg
+        
+    def joint_fl_state_cb(self, msg):
+        if self.prev_fl_joint_msg is not None:
+            self.dFL += msg.position[0] - self.prev_fl_joint_msg.position[0]
+        self.prev_fl_joint_msg = None
+        # self.cur_fl_joint_msg = msg
+        
+    def update_localization(self):
+        # dFL, dFR, dBL, dBR = 0.0, 0.0, 0.0, 0.0
+        
+        # if self.prev_fl_joint_msg is not None:
+        #     dFL = self.cur_fl_joint_msg.position[0] - self.prev_fl_joint_msg.position[0]
+        #     dBL = self.cur_bl_joint_msg.position[0] - self.prev_bl_joint_msg.position[0]
+        #     dBR = self.cur_br_joint_msg.position[0] - self.prev_br_joint_msg.position[0]
+        #     dFR = self.cur_fr_joint_msg.position[0] - self.prev_fr_joint_msg.position[0]
+            
+        # # print(dFL, dFR, dBL, dBR)
+        
+        # self.prev_fr_joint_msg = self.cur_fr_joint_msg
+        # self.prev_fl_joint_msg = self.cur_fl_joint_msg
+        # self.prev_br_joint_msg = self.cur_br_joint_msg
+        # self.prev_bl_joint_msg = self.cur_bl_joint_msg
+        
+        dFL, dFR, dBL, dBR = self.dFL, self.dFR, self.dBL, self.dBR
+            
         dForward = (dFL + dFR + dBL + dBR) / 4.0
         dRight = (-dBL + dBR + dFL -dFR) / 4.0
         
@@ -243,15 +347,14 @@ class MainNode(Node):
         
         self.robot_map[yInPixels][xInPixels] = 255
         
-        cv2.imshow("robotmap", self.robot_map)
-        # Close the window when 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            rclpy.shutdown()
-            exit()
+        self.dFL, self.dFR, self.dBL, self.dBR = 0.0, 0.0, 0.0, 0.0
         
-        self.prev_joint_msg = msg
-        
+        # cv2.imshow("robotmap", self.robot_map)
+        # # Close the window when 'q' is pressed
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     cv2.destroyAllWindows()
+        #     rclpy.shutdown()
+        #     exit()
 
     def __del__(self):
         self.thread_exited = True
